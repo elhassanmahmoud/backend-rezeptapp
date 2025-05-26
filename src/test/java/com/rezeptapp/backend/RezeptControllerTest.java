4f8d478945b461ee3d46b31679a1c6eae4781aed
elhassanmahmoud <mahmoudlhassan63@gmail.com>
2025-05-26 10:55:25 +0200

    backend rezepte db - 2

--- src/test/java/com/rezeptapp/backend/RezeptControllerTest.java
+++ src/test/java/com/rezeptapp/backend/RezeptControllerTest.java
@@ -25,7 +25,7 @@ public class RezeptControllerTest {
     private RezeptService service;
 
     @Test
-    public void testGetRoute() throws Exception {
+    public void testGetSingleRezept() throws Exception {
         Rezept rezept = new Rezept();
         rezept.setId(1L);
         rezept.setName("Spaghetti");
@@ -42,5 +42,36 @@ public class RezeptControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(content().string(containsString("Spaghetti")));
+    }
+
+    @Test
+    public void testGetAllRezepte() throws Exception {
+        Rezept rezept1 = new Rezept();
+        rezept1.setId(1L);
+        rezept1.setName("Pizza");
+        rezept1.setKategorie("Italienisch");
+        rezept1.setBild("pizza.jpg");
+        rezept1.setBeschreibung("Klassisch italienisch");
+        rezept1.setFavorit(true);
+        rezept1.setZutaten(List.of(Map.of("name", "Teig", "menge", "1")));
+        rezept1.setNaehrwerte(Map.of("kcal", 800));
+
+        Rezept rezept2 = new Rezept();
+        rezept2.setId(2L);
+        rezept2.setName("Lasagne");
+        rezept2.setKategorie("Auflauf");
+        rezept2.setBild("lasagne.jpg");
+        rezept2.setBeschreibung("Mit Béchamel");
+        rezept2.setFavorit(false);
+        rezept2.setZutaten(List.of(Map.of("name", "Hack", "menge", "250g")));
+        rezept2.setNaehrwerte(Map.of("kcal", 1000));
+
+        when(service.getAll()).thenReturn(List.of(rezept1, rezept2));
+
+        this.mockMvc.perform(get("/rezepte"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(containsString("Pizza")))
+                .andExpect(content().string(containsString("Lasagne")));
     }
 }
